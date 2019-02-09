480c0173cc041b370eebf556bcfc9b2a3056793b
galubezzz <galubezzz@gmail.com>
2019-02-09 01:11:11 -0800

    added filter for items in order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,12 @@ import React from 'react';
 import OrderItem from './OrderItem/OrderItem'
 
 function Order(props){
+    const newItems = props.items.filter(item => item.count>0);
     return(
         <div>
             Order Details:
-            {props.items.map(
-                item => <OrderItem name={item.name} price={item.price} count={item.count} removeItem ={()=>{props.removeItem(item.name)}}/>
+            {newItems.map(
+                item => <OrderItem name={item.name} price={item.price} count={item.count} removeItem ={()=>{props.removeItem(item.name)}} />
             )}
             Total: {props.total}
         </div>
