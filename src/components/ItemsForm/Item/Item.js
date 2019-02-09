7693297ebb54b16f066e27392f2b08c42218d20b
galubezzzgalubezzz <a@a.com>
2019-02-08 21:45:54 -0800

    added components for item and itemsForm

---
+++ src/components/ItemsForm/Item/Item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Item(props){
+    <div>
+        {props.name}
+        {props.price}
+    </div>
+}
+
+export default Item;
---
+++ src/components/ItemsForm/ItemsForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Item from './Item/Item'
+
+function ItemsForm(props){
+    <div>
+        <Item name='item' price='price'/>
+    </div>
+}
+
+export default ItemsForm;
