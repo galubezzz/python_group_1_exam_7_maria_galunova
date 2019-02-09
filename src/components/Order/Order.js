b656aff1b23e6d165a5841a9f2cde6a43cc15c8b
galubezzzgalubezzz <a@a.com>
2019-02-08 22:01:42 -0800

    added components for Order Details

---
+++ src/components/Order/Order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import OrderItem from './OrderItem/OrderItem'
+
+function Order(props){
+    <div>
+        <OrderItem name='coffee' price={20} count={3} />
+    </div>
+}
+
+export default Order;
---
+++ src/components/Order/OrderItem/OrderItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function OrderItem(props){
+    <div>
+        {props.name}{props.price}{props.count}
+    </div>
+}
+
+export default OrderItem;
